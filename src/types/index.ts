87ab1b61f373214a015d7e4b07c5b052ff8c29b5
Daniel hernandez galvez <hernandezgalvezalejandro@gmail.com>
2025-10-08 14:17:02 -0600

    conection with backend

---
+++ src/types/index.ts
@@ -0,0 +1,60 @@
+import {z} from 'astro:content'
+
+const imageSchema = z.object({
+    url: z.string(),
+    width: z.number(),
+    height: z.number(),
+})
+
+const featureImagesSchema = z.object({
+    thumbnail: imageSchema,
+    medium: imageSchema,
+    medium_large: imageSchema,
+    large: imageSchema,
+    full: imageSchema
+})
+
+export const BaseWPSchema =  z.object({
+    id: z.number(),
+    title: z.object({
+        rendered: z.string()
+    }),
+     content: z.object({
+        rendered: z.string()
+    }),
+    featured_images: featureImagesSchema,
+    acf: z.object({
+        subtitle: z.string()
+    })
+})
+
+const processSchema = z.object({
+    titulo: z.string(),
+    descripcion: z.string(),
+    image: z.string()
+})
+
+export const ProcessPageSchema = BaseWPSchema.extend({
+    acf: z.object({
+        subtitle: z.string()
+    }).catchall(processSchema)
+})
+
+/* schema blog post */
+
+const CategorySchema = z.object({
+    name: z.string(),
+    slug: z.string()
+})
+const CategoriesSchema = z.array(CategorySchema)
+
+export const Postchema = BaseWPSchema.omit({
+    acf: true // esto va a tomar todas las porpiedades del basewpschema menos acf
+}).extend({
+    date: z.string(),
+    category_details: CategoriesSchema
+})
+
+export const PostsSchema = z.array(Postchema)
+
+export type Post = z.infer<typeof Postchema>
